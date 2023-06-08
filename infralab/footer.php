fa295bc26490c7c9c5bbe286dd1b719ae08cb89c
Ulrike Uhlig <u@451f.org>
2023-06-08 12:48:43 +0000

    dont call js just yet

--- infralab/footer.php
+++ infralab/footer.php
@@ -3,7 +3,6 @@
 
     </div>
     <canvas id="canvas" width="600" height="600"></canvas>
-    <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/init.js"></script>
     <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/draw.js"></script>
     <?php wp_footer();  ?>
 </body>
