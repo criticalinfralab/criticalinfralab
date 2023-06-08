561305652cda3286f28362c65521913dd78b0a63
Ulrike Uhlig <u@451f.org>
2023-06-08 13:35:30 +0200

    add canvas and script

--- infralab/footer.php
+++ infralab/footer.php
@@ -1,6 +1,10 @@
     </div><!-- end container -->
     <div id="footer">
+
+    </div>
+    <canvas id="canvas" width="600" height="600"></canvas>
     <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/init.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/draw.js"></script>
     <?php wp_footer();  ?>
 </body>
 </html>
