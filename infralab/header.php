b299295def3b6f95ea10417202a9aaf41ca1e68e
Ulrike Uhlig <u@451f.org>
2023-06-09 16:05:16 +0000

    revalidate css, circumvent cache

--- infralab/header.php
+++ infralab/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="icon" type="image/png" href="<?php get_bloginfo('stylesheet_directory')."/images/favicon.png"; ?>" />
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all" />
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?revalidate" type="text/css" media="all" />
     <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
     <?php wp_head(); ?>
 </head>
