e53e852838c0e1e6e577f0a94ce93f141e985612
Ulrike Uhlig <u@451f.org>
2023-06-12 17:24:38 +0000

    change link style

--- infralab/nav.php
+++ infralab/nav.php
@@ -1,7 +1,7 @@
-<div class="navidation">
+<div class="navigation">
 <?php if (is_single()) : ?>
-    <div class="previous"><?php previous_post_link() ?></div>
-    <div class="next"><?php next_post_link() ?></div>
+    <div class="previous"><?php previous_post_link('%link') ?></div>
+    <div class="next"><?php next_post_link('%link') ?></div>
 <?php else: ?>
     <span class="next"><?php next_posts_link() ?></span>
     <span class="previous"><?php previous_posts_link() ?></span>
