859a975d8d68811fa901202bf1631f38318e4181
Ulrike Uhlig <u@451f.org>
2023-06-12 13:37:54 +0000

    date for blog posts

--- infralab/page_home.php
+++ infralab/page_home.php
@@ -38,7 +38,11 @@ Template Name: Homepage
                                 <?php if(has_tag('has-recording')): ?>
                                    <span class="has-recording">📹 <!-- has link to a recording --></span>
                                 <?php endif; ?>
-	                        <h4 class="item-title"><em class="category"><?php echo $category[0]->cat_name; ?></em><span><?php the_title(); ?></span></h4>
+	                        <h4 class="item-title">
+                                    <em class="category"><?php echo $category[0]->cat_name; ?></em>
+                                    <span class="title"><?php the_title(); ?></span>
+                                    <span class="date"><?php if($category[0]->cat_name != "reading group"): the_time('F Y'); endif; ?></span>
+                                </h4>
 	                        <div class="item-content"><?php the_content(); ?></div>
 	                   </div>
 	                  <?php endwhile;
