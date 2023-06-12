2977b2597ad9da53c25a57160e098ba6803c5923
Ulrike Uhlig <u@451f.org>
2023-06-12 17:25:33 +0000

    remove obsolete file

--- infralab/post-content-single.php
+++ infralab/post-content-single.php
@@ -4,7 +4,7 @@
        <span class="has-recording">📹 <!-- has link to a recording --></span>
     <?php endif; ?>
     <h2 class="item-title">
-        <em class="category"><?php echo $category[0]->cat_name; ?></em>
+        <em class="section-title"><?php echo $category[0]->cat_name; ?></em>
         <span class="title"><?php the_title(); ?></span>
         <span class="date"><?php if($category[0]->cat_name != "reading group"): the_time('F Y'); endif; ?></span>
     </h2>
