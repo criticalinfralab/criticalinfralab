d8d8f33764cb892011576ea062144f86e8b2f2d0
Ulrike Uhlig <u@451f.org>
2023-06-13 14:55:00 +0000

    deal better with empty elements

--- infralab/post-content.php
+++ infralab/post-content.php
@@ -1,6 +1,6 @@
 <?php $category = get_the_category(); ?>
-<div class="item">
-        <a class="permalink" href="<?php the_permalink(); ?>" title="permanent link to <?php the_title(); ?>">#</a>
+<div class="item<?php if ( empty( get_the_content() ) ): echo " empty-content"; endif; ?>">
+    <a class="permalink" href="<?php the_permalink(); ?>" title="permanent link to <?php the_title(); ?>">#</a>
     <?php if(has_tag('has-recording')): ?>
        <span class="has-recording" title="This item has a link to a recording.">📹 <!-- has link to a recording --></span>
     <?php endif; ?>
@@ -9,5 +9,7 @@
         <span class="title"><?php the_title(); ?></span>
         <span class="date"><?php if($category[0]->cat_name != "reading group"): the_time('F Y'); endif; ?></span>
     </h4>
+    <?php if ( !empty( get_the_content() ) ): ?>
     <div class="item-content hidden"><?php the_content(); ?></div>
+    <?php endif; ?>
 </div>
