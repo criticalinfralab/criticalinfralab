0c37366f4c6992f61f74ba20e66c0725489c1437
Ulrike Uhlig <u@451f.org>
2023-06-15 12:26:35 +0000

    move scripts to functions + lazyload

--- infralab/footer.php
+++ infralab/footer.php
@@ -1,9 +1,9 @@
     </div><!-- end container -->
     <div id="footer">
-	<img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-UvA.svg" alt="logo University of Amsterdam" />
-	<img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-fordfoundation.svg" alt="logo Ford Foundation" />
-	<img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-isocfoundation.svg" alt="logo ISOC Foundation" />
-	<img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-omidyar-network.svg" alt="logo Omidyar Network" />
+	<img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-UvA.svg" alt="logo University of Amsterdam" loading="lazy" />
+	<img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-fordfoundation.svg" alt="logo Ford Foundation" loading="lazy" />
+	<img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-isocfoundation.svg" alt="logo ISOC Foundation" loading="lazy" />
+	<img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-omidyar-network.svg" alt="logo Omidyar Network" loading="lazy" />
     </div>
     <canvas id="canvas" width="600" height="600"></canvas>
     <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/vivus.min.js"></script>
