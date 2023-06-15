a439f3cda65537684e0f845d149ef0e7f5a0801f
Ulrike Uhlig <u@451f.org>
2023-06-15 10:51:36 +0000

    remove this line, favicon will be added through interface by WP

--- infralab/header.php
+++ infralab/header.php
@@ -4,7 +4,6 @@
     <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
     <link rel="profile" href="http://gmpg.org/xfn/11" />
-    <link rel="icon" type="image/png" href="<?php get_bloginfo('stylesheet_directory')."/images/favicon.png"; ?>" />
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?revalidate" type="text/css" media="all" />
     <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
     <?php wp_head(); ?>
