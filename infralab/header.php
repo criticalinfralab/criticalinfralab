67181d5b75712a8666c12a3caa3574bc202a3b03
Ulrike Uhlig <u@451f.org>
2023-06-16 10:34:15 +0000

    fix W3C complaints, hide menu

--- infralab/header.php
+++ infralab/header.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
+<html lang="en">
 <head>
     <title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> <?php } ?> <?php wp_title(); ?></title>
-    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
+    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes">
     <link rel="profile" href="http://gmpg.org/xfn/11" />
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?revalidate" type="text/css" media="all" />
-    <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?revalidate" type="text/css" media="all">
+    <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>">
     <?php wp_head(); ?>
 </head>
 
@@ -18,14 +19,14 @@
         </h1>
         <div id="antennaspace"><!-- space for antennas --></div>
         <a class="button" id="eyecare" href="#">desaturate</a>
-        
+        <!--
         <input type="checkbox" id="menuToggle">
         <label for="menuToggle" class="menu-icon">
-           <div class="toggler">
+           <span class="toggler">
                <span aria-hidden="true" class="menu-deco"></span>
                <span aria-hidden="true" class="menu-deco"></span>
                <span aria-hidden="true" class="menu-deco"></span>
-           </div>
+           </span>
         </label>
         <ul class="menu">
             <li><a href="<?php bloginfo('url'); ?>/#header">start</a></li>
@@ -33,5 +34,6 @@
             <li><a href="<?php bloginfo('url'); ?>/#section-publications">publications</a></li>
             <li><a href="<?php bloginfo('url'); ?>/#section-people-and-governance">people and governance</a></li>
         </ul>
+        -->
     </div>
     <div id="container">
