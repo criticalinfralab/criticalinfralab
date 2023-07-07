f1a4def96e31097501ff92408ee98df034f405f0
Ulrike Uhlig <u@451f.org>
2023-07-07 10:57:20 +0000

    show all activities on this page

--- infralab/page_activities.php
+++ infralab/page_activities.php
@@ -13,29 +13,17 @@ Template Name: Activities
 	<?php endwhile; ?>
 	    <div class="section-content">
 	        <?php // blogposts
-                $count = get_option('posts_per_page', 5);
-                $paged = get_query_var('paged') ? get_query_var('paged') : 1;
 	        $postquery = new WP_Query( array(
 	            'post-type' => 'post',
 	            'post_status'  => 'publish',
 	            'sort_order' => 'ASC',
-                    'posts_per_page' => $count,
-                    'paged' => $paged
+                    'posts_per_page' => -1,
 	        ));
                 if ($postquery->have_posts()) : 
                     while ($postquery->have_posts()) : $postquery->the_post();
                          get_template_part('post-content');
 	            endwhile;
 	        endif;
-                // Pagination
-                if($postquery->max_num_pages > 1) {
-                    if($postquery->query_vars["paged"] == 0) {
-                        $current_page = 1;
-                    } else {
-                        $current_page = $postquery->query_vars["paged"];
-                    }
-                    echo '<div class="pagination" data-query="'.htmlspecialchars(json_encode($postquery->query_vars)).'" data-maxpages="'.htmlspecialchars(json_encode($postquery->max_num_pages)).'" data-current="'.$current_page.'">'.paginate_links(array('total' => $postquery->max_num_pages)).'</div>';
-                }
 	        wp_reset_postdata(); ?>
 	    </div>
         <?php endif; ?>
