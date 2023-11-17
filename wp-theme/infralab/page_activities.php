a899de758daa573800f106ba9b7e2dac93019d00
Ulrike Uhlig <u@451f.org>
2023-11-17 18:56:24 +0100

    indent

--- wp-theme/infralab/page_activities.php
+++ wp-theme/infralab/page_activities.php
@@ -7,10 +7,10 @@ Template Name: Activities
     <div id="content" class="content">
         <?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post(); ?>
-       <div class="section" id="section-<?php echo $post->post_name; ?>">
-           <h2 class="section-title"><?php the_title(); ?></h2>
-               <a class="go-home" href="<?php bloginfo('url'); ?>">←</a>
-    <?php endwhile; ?>
+        <div class="section" id="section-<?php echo $post->post_name; ?>">
+            <h2 class="section-title"><?php the_title(); ?></h2>
+            <a class="go-home" href="<?php bloginfo('url'); ?>">←</a>
+        <?php endwhile; ?>
         <div class="section-content">
             <?php // blogposts
                 $postquery = new WP_Query( array(
