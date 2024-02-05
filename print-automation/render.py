dab90a420c4e411e0765cabef0373073e9ce76f4
maxigas <maxigas@puscii.nl>
2024-02-05 16:04:34 +0100

    Reformat printed messages

--- print-automation/render.py
+++ print-automation/render.py
@@ -8,26 +8,41 @@ def assert_debian_based():
     if os.path.isfile('/etc/debian_version'):
         print("Found Debian-based GNU/Linux distro.  Good!")
     else:
-        exit("""You need a Debian-based GNU/Linux distro to run this script.
-ERROR Incompatible OS: Not a Debian-based GNU/Linux operating system distribution""")
+        exit("""
+        This script only runs on Debian-based GNU/Linux distros.
+        ERROR Incompatible OS:
+              Not a Debian-based GNU/Linux operating system distribution
+        """)
 
 def assert_correct_number_of_arguments():
     if len(sys.argv) > 2:
-        exit("Illegal number of parameters.  Render accepts zero or one argument.  The optional argument is the serial number of the CIL publication (1-999) that points to a relevant directory like CIL001 or CIL999.")
+        exit("""
+        The first and only argument should be an integer or nothing.
+        ARGUMENT ERROR:
+                 Illegal number of parameters.
+                 Render accepts zero or one argument.
+                 The optional argument is the serial number
+                 of the CIL publication (1-999) that points
+                 to a relevant directory like CIL001 or CIL999.
+        """)
 
 def assert_correct_type_of_arguments():
     if len(sys.argv) == 2:
         try:
             int(sys.argv[1])
         except:
-            raise ValueError("""ERROR: First parameter should be a number (e.g. an integer).
-
-Render accepts zero or one argument.  The optional argument is the serial number of the CIL publication (1-999) that points to a relevant directory like CIL001 or CIL999.""")
+            exit("""
+            VALUE ERROR:
+                  First parameter should be a number (e.g. an integer).
+                  Render accepts zero or one argument.
+                  The optional argument is a serial number of a CIL publication (1-999)
+                  that points to a relevant directory like CIL001 or CIL999.
+            """)
 
 def assert_sudo_capability():
     sudo, cmd = False, subprocess.run(['sudo', '-l'], capture_output=True)
     for line in cmd.stdout.decode().split('\n'):
-        if "ALL" in line:
+        if ("ALL" in line) or ("root" in line):
            sudo = True
     if not sudo:
         exit("""You need sudo rights to run this script.
