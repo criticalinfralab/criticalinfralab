9cde23207f77954ddb253d0a2bb7c0996c1dca6a
Ulrike Uhlig <u@451f.org>
2024-04-02 16:57:41 +0200

    add new logos to footer

--- wp-theme/infralab/footer.php
+++ wp-theme/infralab/footer.php
@@ -1,9 +1,10 @@
     </div><!-- end container -->
     <div id="footer">
-	<img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-UvA.svg" alt="logo University of Amsterdam" loading="lazy">
-	<img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-fordfoundation.svg" alt="logo Ford Foundation" loading="lazy">
-	<img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-isocfoundation.svg" alt="logo ISOC Foundation" loading="lazy">
-	<img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-omidyar-network.svg" alt="logo Omidyar Network" loading="lazy">
+    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-UvA.svg" alt="logo University of Amsterdam" loading="lazy">
+    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-fordfoundation.svg" alt="logo Ford Foundation" loading="lazy">
+    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-isocfoundation.svg" alt="logo ISOC Foundation" loading="lazy">
+    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-otf.svg" alt="logo Open Technology Fund" loading="lazy">
+    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-nwo.svg" alt="logo Nederlandse Organisatie voor Wetenschappelijk Onderzoek" loading="lazy">
     </div>
     <canvas id="canvas" width="600" height="600"></canvas>
     <script src="<?php bloginfo('stylesheet_directory'); ?>/js/vivus.min.js"></script>
