c9ed3d19f85c4adab3b9b049324eabce5c706e20
Ulrike Uhlig <u@451f.org>
2024-04-02 17:06:54 +0200

    order alphabetically

--- wp-theme/infralab/footer.php
+++ wp-theme/infralab/footer.php
@@ -1,10 +1,10 @@
     </div><!-- end container -->
     <div id="footer">
-    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-UvA.svg" alt="logo University of Amsterdam" loading="lazy">
-    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-fordfoundation.svg" alt="logo Ford Foundation" loading="lazy">
-    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-isocfoundation.svg" alt="logo ISOC Foundation" loading="lazy">
-    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-otf.svg" alt="logo Open Technology Fund" loading="lazy">
-    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-nwo.svg" alt="logo Nederlandse Organisatie voor Wetenschappelijk Onderzoek" loading="lazy">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-fordfoundation.svg" alt="logo Ford Foundation" loading="lazy">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-isocfoundation.svg" alt="logo ISOC Foundation" loading="lazy">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-nwo.svg" alt="logo Nederlandse Organisatie voor Wetenschappelijk Onderzoek" loading="lazy">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-otf.svg" alt="logo Open Technology Fund" loading="lazy">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-UvA.svg" alt="logo University of Amsterdam" loading="lazy">
     </div>
     <canvas id="canvas" width="600" height="600"></canvas>
     <script src="<?php bloginfo('stylesheet_directory'); ?>/js/vivus.min.js"></script>
